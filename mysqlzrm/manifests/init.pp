4edefe06758d029db700bf09da8b75f5050c284c
Alston Fernandez <alston.fernandez@gmail.com>
2013-04-01 12:50:07 -0400

    added new template to mysqlzrm

--- mysqlzrm/manifests/init.pp
+++ mysqlzrm/manifests/init.pp
@@ -1,6 +1,28 @@
-class mysql-zrm {
-	package { "MySQL-zrm":
+class mysql-zrm (
+		$backup_level = '0',
+                $backup_mode  = 'logical',
+                $backup_type  = 'regular',
+                $destination  = '/data/backup/mysql',
+                $retention    = "",
+                $compress     = '1',
+                $databases    = [],
+                $user         = "",
+                $password     = "",
+                $host         = "localhost",
+                $mailto       = "alston.fernandez@gmail.com",
+		){
+
+ file {"/etc/mysql-zrm/mysql-zrm.conf":
+        owner   => "root",
+        group   => "root",
+        mode    => "0600",
+        content => template("mysqlzrm/mysql-zrm.conf.erb"),
+        require => Package["MySQL-zrm"],
+ }
+
+ package { "MySQL-zrm":
         ensure => 'installed',
         provider => 'yum',
-	}
+ }
+
 }
